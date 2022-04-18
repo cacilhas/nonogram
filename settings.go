a037a5426f384c2ce7988cd5134e16299987d6db
Arhimedeς Montegasppα <cacilhas@valfenda.local>
2022-04-18 11:08:30 -0300

    release 2.7

--- settings.go
+++ settings.go
@@ -21,7 +21,7 @@ func readSettings() {
 	}
 	defaultSettings()
 	viper.Set("homepage", "https://cacilhas.itch.io/nonogram")
-	viper.Set("version", "2.nightly")
+	viper.Set("version", "2.7")
 }
 
 func defaultSettings() {

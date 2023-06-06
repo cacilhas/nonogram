f6639b6add0a14653b7bccc2ae55fddbdcee0f49
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-06-06 10:51:40 -0300

    use rscenes::prelude::mem

--- src/audio.rs
+++ src/audio.rs
@@ -1,5 +1,3 @@
-use std::ffi::CString;
-
 use rscenes::prelude::*;
 
 #[derive(Clone, Copy)]
@@ -46,25 +44,9 @@ impl Sfx {
     }
 
     fn load_sound(bytes: &'static [u8]) -> Option<Sound> {
-        let data = bytes.iter().map(|e| e.to_owned()).collect::<Vec<u8>>();
-        match unsafe { Sfx::load_wave_from_mem(".wav", &data, data.len() as i32) } {
-            Err(_) => None,
-            Ok(wave) => Sound::load_sound_from_wave(&wave).ok(),
-        }
-    }
-
-    // TODO: simplify this
-    unsafe fn load_wave_from_mem(
-        filetype: &str,
-        bytes: &Vec<u8>,
-        size: i32,
-    ) -> Result<Wave, String> {
-        let c_filetype = CString::new(filetype).unwrap();
-        let c_bytes = bytes.as_ptr();
-        let w = ffi::LoadWaveFromMemory(c_filetype.as_ptr(), c_bytes, size);
-        if w.data.is_null() {
-            return Err(format!("Wave data is null. Check provided buffer data"));
-        };
-        Ok(Wave::from_raw(w))
+        let wav: &str = mem::WaveType::Wav.into();
+        mem::load_wave(wav, bytes)
+            .ok()
+            .and_then(|wave| Sound::load_sound_from_wave(&wave).ok())
     }
 }

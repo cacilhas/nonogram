65c7ce136a69e49673d135bb32321e8666819f4c
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-06-06 11:24:31 -0300

    simplify installation

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! # Install
 //!
 //! ```sh
-//! cargo install kodumaro-nonogram --bin nonogram
+//! cargo install kodumaro-nonogram
 //! ```
 //!
 //! If you have [UPX](https://upx.github.io/) installed, you maybe want to run:
